bb75243c484c66351c6705290c197c0f91f9491f
Anael <anael.lefebvre@viacesi.fr>
2017-10-23 16:34:51 +0400

    implementing promise

--- server/boot/mosca.js
+++ server/boot/mosca.js
@@ -12,7 +12,6 @@ module.exports = function(app) {
   
   server.on('clientConnected', function(client) {
     app.models.client.create({objectId :client.id});
-    
     console.log('client connected', client.id);		
   });
   // fired when a client disconnects
@@ -23,31 +22,36 @@ module.exports = function(app) {
   });
   // fired when a message is received
   server.on('published', function(packet, client) {
-    // console.log('packet', packet);
     if (client){
-      var isConnected = app.models.client.find({where: {objectId: client.id}},
-        (err, results) => {
-          if(!err && results){
-            // console.log('results', results);
-            app.models.topic.findOrCreate({where: {name: packet.topic}}, {name: packet.topic},
-            (err, instance, created) => {
-              if(!err){
-                app.models.message.create({text: packet.payload, idTopic: instance.__data.id},
-                (err) => {
-                  if(err) console.log(err);
-                });
-              }
-            });
-          }
+      getPersistedTopic(packet.topic).then(
+        (topic) => {
+          return addMessageToTopic(packet.payload, topic.id);
         }
       );
     }
-    
-    console.log('Published', packet.payload);
   });
   
   // fired when the mqtt server is ready
   function setup() {
     console.log('Mosca server is up and running')
+  }
+
+  function getPersistedTopic (name){
+    return app.models.topic.findOrCreate({where: {name: name}}, {name:name}).then(
+      (res) => {
+        var instance = res[0], created = res [1];
+        if(created) console.log("new Topic created ", instance.__data);
+        return instance.__data;
+      }
+    );
+  }
+
+  function addMessageToTopic(msg, topicId){
+    return app.models.message.create({text: msg, idTopic: topicId}).then(
+      res => {
+        console.log("new message created ", res.__data);
+        return res.__data;
+      }
+    );
   }
 };
